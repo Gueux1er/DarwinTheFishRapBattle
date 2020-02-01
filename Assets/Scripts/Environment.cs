6e7c91cd44b67a78b4b7d18ddaaa4bcde9a5f537
Clement <clement.roth@live.fr>
2020-02-01 12:44:52 +0100

    Optimize crowd animation

---
+++ Assets/Scripts/Environment.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Environment : MonoBehaviour
+{
+    public Vector3 startPosition;
+    public Vector3 endPosition;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position + startPosition, 0.5f);
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position + endPosition, 0.4f);
+    }
+}
